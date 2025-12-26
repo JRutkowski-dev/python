3a2e2ce68513600f928c6cd9a9e3352f66b98f34
Jakub Rutkowski <polaris.kuba@gmail.com>
2025-12-26 02:21:00 +0100

    trzecia wersja kody wyliczajacego liczbe Pi

--- pi-to-the-Nth.py
+++ pi-to-the-Nth.py
@@ -1,8 +1,11 @@
-import math
+import mpmath import mp
 
 Pi = math.pi
 
 
-ans = int(input("podaj do ktorej liczby po przecinku chcialbys wyliczyc z Pi(max 14)"))
+n = int(input("podaj do ktorej liczby po przecinku chcialbys wyliczyc z Pi(max 14)"))
 
-print (f"{Pi:.{ans+1}}")
+mp.dps = n + 1
+
+
+print (mp.pi)
