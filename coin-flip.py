5aebe077e29ced2f8156a80af769c13a6d7e68c9
Jakub Rutkowski <polaris.kuba@gmail.com>
2025-12-28 01:30:14 +0100

    pierwsza werska kodu

---
+++ coin-flip.py
@@ -0,0 +1,15 @@
+import random
+
+def coinflip():
+	number = input("ile razy chcesz rzucic moneta: ")
+	recordlist = []
+	orzel = 0
+	reszka = 0
+	flip = random.randint(0, 1)
+	if (flip == 0):
+		print ("orzel")
+		recordlist.append("orzel")
+	else:
+		print ("reszka")
+		recordlist.append("reszka")
+	print (str(recordlist.count("orzel")) + str(recordlist.count("reszka")))
